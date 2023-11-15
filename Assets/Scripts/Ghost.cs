aecc712db21f8d31e6dca356bdd1b2b0653de475
Arthur Prediger <arthurprediger@edu.unisinos.br>
2023-11-15 17:00:27 -0300

    Simple ghost and hunter prefabs for better prototyping.

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class Ghost : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float moveSpeed = 16.0f;
---
+++ Assets/Scripts/Hunter.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Hunter : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float moveSpeed = 16.0f;
+
+    [SerializeField]
+    private InputActionReference movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        Vector2 direction = movement.action.ReadValue<Vector2>().normalized;
+        Vector2 moveAmount = moveSpeed * Time.deltaTime * direction;
+
+        rb.position += moveAmount;
+    }
+}
