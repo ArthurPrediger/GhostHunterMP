d53b5ffd6ad35fe070e4cc3b642f5756193b0be7
Arthur Prediger <arthurprediger@edu.unisinos.br>
2023-11-22 12:44:42 -0300

    Ghost dash added.

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,7 +8,10 @@ public class Ghost : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float moveSpeed = 16.0f;
+    Vector2 dashDirection = Vector2.zero;
+    public float dashSpeed = 8.0f;
     private Animator animator;
+    bool canMove = true;
     bool canAttack = true;
     bool canDash = true;
 
@@ -28,30 +32,35 @@ public class Ghost : MonoBehaviour
     {
         MoveAction();
         AttackAction();
+        DashAction();
     }
 
     public void MoveAction()
     {
+        if (!movement.action.IsPressed() || !canMove) return;
+
         Vector2 direction = movement.action.ReadValue<Vector2>().normalized;
         Vector2 moveAmount = moveSpeed * Time.deltaTime * direction;
 
         rb.position += moveAmount;
 
-        if(Mathf.Abs(direction.x) > 0.0001f && Mathf.Abs(direction.y) < 0.0001f)
+        if(Mathf.Abs(direction.x) > 0f && direction.y == 0f)
         {
             animator.SetInteger("Rotation", 2);
 
-            if(direction.x < -0.0001f) transform.localScale = new Vector3(-1f, 1f, 1f);
+            if(direction.x < 0f) transform.localScale = new Vector3(-1f, 1f, 1f);
             else transform.localScale = new Vector3(1f, 1f, 1f);
         }
-        else if(direction.y > 0.0001f)
+        else if(direction.y > 0f)
         {
             animator.SetInteger("Rotation", 1);
         }
-        else if(direction.y < -0.0001f)
+        else if(direction.y < 0f)
         {
             animator.SetInteger("Rotation", 0);
         }
+
+        dashDirection = direction;
     }
 
     public void AttackAction()
@@ -60,7 +69,7 @@ public class Ghost : MonoBehaviour
 
         if (!attacking || !canAttack) return;
 
-        canAttack = false;
+        SetAllActionsState(false);
 
         animator.SetBool("IsAttacking", true);
         StartCoroutine(AttackCooldown());
@@ -74,6 +83,55 @@ public class Ghost : MonoBehaviour
         yield return new WaitForSeconds(waitActivation);
         animator.SetBool("IsAttacking", false);
         yield return new WaitForSeconds(attackDuration - waitActivation);
-        canAttack = true;
+
+        SetAllActionsState(true);
+    }
+
+    private void DashAction()
+    {
+        bool dashing = dash.action.WasPressedThisFrame();
+
+        if (!dashing || !canDash) return;
+
+        SetAllActionsState(false);
+
+        animator.SetBool("IsDashing", true);
+        StartCoroutine (DashUpdate());
+        StartCoroutine(DashCooldown());
+    }
+
+    private IEnumerator DashUpdate() 
+    {
+        float dashDuration = 1.25f / 8f;
+        float time = 0f;
+        while(time < dashDuration)
+        {
+            rb.transform.position += dashSpeed * Time.deltaTime * (Vector3)dashDirection;
+            time += Time.deltaTime;
+            //UnityEngine.Debug.Log(time);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private IEnumerator DashCooldown()
+    {
+        float dashDuration = 1.25f / 8f;
+        float waitActivation = dashDuration / 4.0f;
+
+        yield return new WaitForSeconds(waitActivation);
+        animator.SetBool("IsDashing", false);
+        yield return new WaitForSeconds(dashDuration - waitActivation);
+
+        SetAllActionsState(true);
+        canDash = false;
+        yield return new WaitForSeconds(0f);
+        canDash = true;
+    }
+
+    void SetAllActionsState(bool state)
+    {
+        canMove = state;
+        canAttack = state; 
+        canDash = state;
     }
 }
